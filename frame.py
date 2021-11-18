862fe490ec367d9ab7dc245fc9fd58dde4e93e66
Gergő Bieleczki <72573519+BieleczkiGergo@users.noreply.github.com>
2021-11-18 21:21:17 +0100

    First commit (no, not really)

---
+++ frame.py
@@ -0,0 +1,13 @@
+import _tkinter as tk
+from tkinter import Tk
+
+class game:
+    mainFrame = 0
+    master = 0
+    def __init__(master):
+        pass
+
+
+window = Tk()
+game_main = game(window)
+
