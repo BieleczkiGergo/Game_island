16be0cfda2703618e14b94e218afb71b6de96ce2
Geri <bgergo8b@gmail.com>
2021-12-02 14:05:25 +0100

    continued work on basic sprites

--- frame.py
+++ frame.py
@@ -7,12 +7,22 @@ class mainFrame:
     label1 = 0
     button1 =0
     frames = 0
+    polygon = 0
+    def add_sprite(self, sprite):
+        pass
+
+    def add_polygon(self, x, y, width, height):
+        self.mainFrame.create_polygon(x, y, width+x, height+y)
+
+    def start(self):
+        self.polygon = self.mainFrame.add_polygon(10, 10, 20, 20)
+
     def __init__(self):
         self.master = tk.Tk()
         self.mainFrame = tk.Canvas(self.master, width=600, height=300, relief=tk.GROOVE, borderwidth=2)
         self.others = tk.Frame(self.master, relief=tk.GROOVE, borderwidth=2)
         self.label1 = tk.Label(self.others, text="something: ")
-        self.button1 = tk.Button(self.others, text="Start", command=start)
+        self.button1 = tk.Button(self.others, text="Start", command=self.start)
 
         self.master.minsize(width=800, height=300)
 
@@ -22,14 +32,6 @@ class mainFrame:
         self.mainFrame.pack(side=tk.LEFT, fill=tk.BOTH)
         self.master.mainloop()
 
-    def add_sprite(self, sprite):
-        pass
-
-    def add_polygon(self, x, y, width, height):
-        self.mainFrame.create_polygon(x, y, width+x, height+y)
-
-    def start(self):
-        self.add_polygon(10, 10, 20, 20)
 
 
 
